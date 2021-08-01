0657b16a51a653acbde8284f293355ab9f0aba3a
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2021-08-01 15:57:55 -0300

    ensure WeatherRouter returns 400 if no date is provided

---
+++ src/presentation/WeatherRouter.spec.js
@@ -0,0 +1,24 @@
+class WeatherRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.date) {
+      return {
+        statusCode: 400
+      }
+    }
+    return 200
+  }
+}
+
+describe('Weather Router', () => {
+  test('Should return 400 if no date is provided', () => {
+    const sut = new WeatherRouter()
+    const httpRequest = {
+      body: {
+        city: 'any_city',
+        limit: 'any_limit'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
