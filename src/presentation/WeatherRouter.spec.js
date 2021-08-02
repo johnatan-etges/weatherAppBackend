cd50f6c334cfe2fb7c867f662cff4f0608ac1d57
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2021-08-01 21:18:40 -0300

    ensure WeatherUseCase is caller with correct params

--- src/presentation/WeatherRouter.spec.js
+++ src/presentation/WeatherRouter.spec.js
@@ -1,5 +1,23 @@
+class WeatherUseCase {
+  queryWeather (date, city, limit) {
+    this.date = date
+    this.city = city
+    this.limit = limit
+  }
+}
+
 class WeatherRouter {
+  constructor (weatherUseCase) {
+    this.weatherUseCase = weatherUseCase
+  }
+
   route (httpRequest) {
+    if (!this.weatherUseCase) {
+      return {
+        statusCode: 500
+      }
+    }
+
     if (!httpRequest || !httpRequest.body) {
       return {
         statusCode: 500
@@ -11,12 +29,32 @@ class WeatherRouter {
         statusCode: 400
       }
     }
+
+    this.weatherUseCase.queryWeather('any_date', 'any_city', 'any_limit')
+  }
+}
+
+const makeSut = () => {
+  const weatherUseCase = new WeatherUseCase()
+  const sut = new WeatherRouter(weatherUseCase)
+  return {
+    sut,
+    weatherUseCase
+  }
+}
+
+const makeSutWithError = () => {
+  const weatherUseCase = new WeatherUseCase()
+  const sut = new WeatherRouter()
+  return {
+    sut,
+    weatherUseCase
   }
 }
 
 describe('Weather Router', () => {
   test('Should return 400 if no date is provided', () => {
-    const sut = new WeatherRouter()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         city: 'any_city',
@@ -28,7 +66,7 @@ describe('Weather Router', () => {
   })
 
   test('Should return 400 if no city is provided', () => {
-    const sut = new WeatherRouter()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         date: 'any_date',
@@ -40,7 +78,7 @@ describe('Weather Router', () => {
   })
 
   test('Should return 400 if no limit is provided', () => {
-    const sut = new WeatherRouter()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         date: 'any_date',
@@ -52,15 +90,43 @@ describe('Weather Router', () => {
   })
 
   test('Should return 500 if no httpRequest is provided', () => {
-    const sut = new WeatherRouter()
+    const { sut } = makeSut()
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('Should return 500 if an invalid httpRequest is provided', () => {
-    const sut = new WeatherRouter()
+    const { sut } = makeSut()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if no WeatherUseCase is provided', () => {
+    const { sut } = makeSutWithError()
+    const httpRequest = {
+      body: {
+        date: 'any_date',
+        city: 'any_city',
+        limit: 'any_limit'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should call WeatherUseCause.queryWeather with correct parameters', () => {
+    const { sut, weatherUseCase } = makeSut()
+    const httpRequest = {
+      body: {
+        date: 'any_date',
+        city: 'any_city',
+        limit: 'any_limit'
+      }
+    }
+    sut.route(httpRequest)
+    expect(weatherUseCase.date).toEqual(httpRequest.body.date)
+    expect(weatherUseCase.city).toEqual(httpRequest.body.city)
+    expect(weatherUseCase.limit).toEqual(httpRequest.body.limit)
   })
 })
